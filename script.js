311a83dd0cb3104dd6316ff5c37fd1c17171ca2a
Hype <hudmaulana33@gmail.com>
2025-11-14 10:06:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let array = ["Alifio", "Aliful Daffa", "ganteng"];
+let angka = 2
+document.write(array + "<Br>")
+
+for (let i = 0; i < array.length; i++) {
+        document.write (i + 1 + array [i] + "<br>")
+}
+        
+teman = "ganteng"
+
+if (teman === "Alifio") {
+        document.write(" LEGEND!  Traktir Bakso! ")
+} else if (teman === "Aliful Daffa") {
+        document.write(" KEREN!  Lolos KKM, santai.")
+} else if (teman === "ganteng") {
+        document.write(" GAGAL DEH.  Belajar lagi, Bro/Sis.")
+}
